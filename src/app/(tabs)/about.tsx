4ffb3efd6b82048a37607d0db621f8be23504a12
Duverney Hernandez Mora <du.hernandez@udla.edu.co>
2025-05-22 01:02:31 -0500

    Feat: create the About view to describe the app

--- src/app/(tabs)/about.tsx
+++ src/app/(tabs)/about.tsx
@@ -1,13 +1,74 @@
-import { Colors } from "@/utils/colors";
+import { View, StyleSheet, ScrollView } from "react-native";
 import { Text } from "@rneui/themed";
-import { View } from "react-native";
+import { Colors } from "@/utils/colors";
 
 const CryptoDetailAbout = () => {
     return (
-        <View style={{ flex: 1, backgroundColor: Colors.primary }}>
-            <Text>Crypto About View</Text>
-        </View>
+        <ScrollView style={{ flex: 1, backgroundColor: Colors.primary }}>
+            <Text style={styles.titleStyles}>Crypto Market About</Text>
+
+            <View style={styles.section}>
+                <Text>
+                    This mobile app lets you view cryptocurrency data that's updated every 5 minutes.
+                    Includes views such as:
+                    {"\n"}- Home: Market Overview and Featured Prices.
+                    {"\n"}- Detail: specific information for each cryptocurrency.
+                    {"\n"}- Favorites: Custom coin tracking.
+                    {"\n"}- About: details about the app and its development.
+                    {"\n\n"}
+                    Technologies used:
+                    {"\n"}- React Native for the mobile interface.
+                    {"\n"}- TypeScript for safe typing.
+                    {"\n"}- @rneui/themed for UI Components.
+                    {"\n"}- TansTack Query for API integration.
+                </Text>
+            </View>
+
+            <Text style={styles.titleStyles}>Developer info</Text>
+
+            <View style={styles.section}>
+                <Text style={{ fontWeight: 'bold', fontSize: 18, marginBottom: 8 }}>
+                    Name:
+                </Text>
+                <Text style={{ marginBottom: 12 }}>
+                    Duverney Hernandez Mora
+                </Text>
+                <Text style={{ fontWeight: 'bold', fontSize: 18, marginBottom: 8 }}>
+                    Email:
+                </Text>
+                <Text style={{ marginBottom: 12 }}>
+                    comdujar@gmail.com
+                </Text>
+                <Text style={{ fontWeight: 'bold', fontSize: 18, marginBottom: 8 }}>
+                    Location:
+                </Text>
+                <Text style={{ marginBottom: 12 }}>
+                    Neiva, Colombia
+                </Text>
+                <Text style={{ fontWeight: 'bold', fontSize: 18, marginBottom: 8 }}>
+                    About Me:
+                </Text>
+                <Text>
+                    Passionate software developer focused on creating robust and intuitive mobile applications. Always eager to learn new technologies and contribute to industry and open source projects.
+                </Text>
+            </View>
+        </ScrollView>
     );
 }
+
+const styles = StyleSheet.create({
+    titleStyles: {
+        fontWeight: 'bold',
+        fontSize: 30,
+        textAlign: 'center',
+        marginVertical: 20
+    },
+    section: {
+        backgroundColor: 'rgba(255, 255, 255, 0.03)',
+        padding: 20,
+        borderRadius: 10,
+        marginHorizontal: 20
+    }
+})
 
 export default CryptoDetailAbout;
